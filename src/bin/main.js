4b7c3ec5a814beb1c2aafa7186622d357a4e6ae7
Andy <ely.peddler@googlemail.com>
2023-02-20 21:09:12 +0000

    Fixed selected tab after refresh

--- src/bin/main.js
+++ src/bin/main.js
@@ -3,12 +3,27 @@ const { invoke } = window.__TAURI__.tauri;
 //let items_element;
 
 async function refresh() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  var i, ids;
+  ids = [];
+  let selected_items = document.getElementsByClassName("selected");
+  console.log("before");
+  for (i = 0; i < selected_items.length; i++) {
+    var id = selected_items[i].id;
+    if(id.length > 0) {
+      console.log(id);
+      ids.push(id);
+    }
+  }
   let refresh_button = document.getElementById("button-refresh");
   refresh_button.className = "active";
   let content = document.getElementById("content");
   content.innerHTML = await invoke("refresh");
   refresh_button.className = "passive";
+  console.log("after");
+  for (i = 0; i < ids.length; i++) {
+    console.log(ids[i]);
+    document.getElementById(ids[i]).className += " selected";
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
